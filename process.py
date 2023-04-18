a20be4939a21674b071cf1ba9de8d744f4b90555
tandycell <131168612+tandycell@users.noreply.github.com>
2023-04-18 10:32:03 -0300

    Update process.py

--- process.py
+++ process.py
@@ -1,7 +1,7 @@
 import os
 import openai
 
-openai.api_key = "XXXXXXXXXXXXXXX" # Replace with your key
+openai.api_key = "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" # Replace with your key
 completion = openai.Completion()
 
 start_sequence = "\nTrigan:"
